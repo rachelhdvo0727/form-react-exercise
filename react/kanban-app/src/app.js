c987c3f48efd9bffa17f581c28d76009ec86a518
rachelvo <rach0727@stud.kea.dk>
2020-05-05 14:54:26 +0200

    done task 11

--- react/kanban-app/src/app.js
+++ react/kanban-app/src/app.js
@@ -1,11 +1,28 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React, { useState } from "react";
 import "./index.css";
 import Navi from "./components/navi";
 import Main from "./components/main";
 
 export default function App() {
-  const cards = [
+  // const cards = [
+  //   {
+  //     title: "Make it dynamic",
+  //     list: "todo",
+  //     added: 1588055291061,
+  //     id: 1,
+  //     color: "hotpink",
+  //     assignedTo: ["jofh"],
+  //   },
+  //   {
+  //     title: "Make it dynamic",
+  //     list: "todo",
+  //     added: 1588055091061,
+  //     id: 2,
+  //     color: "lightblue",
+  //     assignedTo: ["jofh", "davi"],
+  //   },
+  // ];
+  const [cards, setCards] = useState([
     {
       title: "Make it dynamic",
       list: "todo",
@@ -22,13 +39,26 @@ export default function App() {
       color: "lightblue",
       assignedTo: ["jofh", "davi"],
     },
-  ];
+  ]);
+
   return (
     <div className="App">
-      <Navi />
+      <Navi></Navi>
       <Main cards={cards} />
+      <button
+        onClick={() =>
+          setCards(
+            cards.concat({
+              title: "OMG, did that just happen?",
+              list: "todo",
+              added: Date.now(),
+              id: Math.random(),
+              color: "lightblue",
+              assignedTo: ["jofh"],
+            })
+          )
+        }
+      >CLick This</button>
     </div>
   );
 }
-
-ReactDOM.render(<App />, document.getElementById("root"));
