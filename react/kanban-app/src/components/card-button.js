d47684000b4a0f069e83b9464d81316d178c5365
rachelvo <rach0727@stud.kea.dk>
2020-05-05 14:15:50 +0200

    done state 1

--- react/kanban-app/src/components/card-button.js
+++ react/kanban-app/src/components/card-button.js
@@ -1,5 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function CardButton() {
-  return <button>Click</button>;
+export default function CardButton(props) {
+  const [clicks, setClicks] = useState(0);
+  function clicked(e) {
+    setClicks(clicks + 1);
+  }
+  return <button onClick={clicked}>{clicks}</button>;
 }
