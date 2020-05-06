b7dad0651bddeb4de804672a253c7cc2b1f0d456
rachelvo <rach0727@stud.kea.dk>
2020-05-06 14:14:29 +0200

    done setInterval

--- distortion-master/script.js
+++ distortion-master/script.js
@@ -2,6 +2,23 @@
 
 window.addEventListener("DOMContentLoaded", init);
 const HTML = {};
+const data = "https://kea-alt-del.dk/kata-distortion/";
 function init() {
   console.log("init");
+  HTML.inqueue;
+  HTML.numberOfStu = document.querySelector("#students");
+  HTML.needle = document.querySelector(".needle");
+  setInterval(getData, 5000);
+}
+async function getData() {
+  const myQueue = await fetch(data);
+  HTML.inqueue = await myQueue.json();
+  showQueue(HTML.inqueue);
+  //console.log(HTML.inqueue.inQueue);
+}
+function showQueue(queue) {
+  console.log("show queue");
+  console.log(HTML.inqueue.inQueue);
+  HTML.numberOfStu.innerHTML = queue.inQueue + " students";
+  HTML.needle.classList.add("move");
 }
