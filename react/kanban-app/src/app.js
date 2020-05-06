107ae9660d14a50c3907ca298c88470d7da75d24
rachelvo <rach0727@stud.kea.dk>
2020-05-06 10:22:41 +0200

    changed buttons' name

--- react/kanban-app/src/app.js
+++ react/kanban-app/src/app.js
@@ -58,7 +58,7 @@ export default function App() {
             })
           )
         }
-      >CLick This</button>
+      >Click This</button>
     </div>
   );
 }
--- react/kanban-app/src/components/card-button.js
+++ react/kanban-app/src/components/card-button.js
@@ -5,5 +5,5 @@ export default function CardButton(props) {
   function clicked(e) {
     setClicks(clicks + 1);
   }
-  return <button onClick={clicked}>{clicks}</button>;
+  return <button onClick={clicked}>You've clicked {clicks}</button>;
 }
