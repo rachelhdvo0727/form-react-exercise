47c7f5295508ab7538e27f868a3c38eaf3666662
rachelvo <rach0727@stud.kea.dk>
2020-05-06 14:52:26 +0200

    done counting to showStudents

--- distortion-master/script.js
+++ distortion-master/script.js
@@ -6,9 +6,19 @@ const data = "https://kea-alt-del.dk/kata-distortion/";
 function init() {
   console.log("init");
   HTML.inqueue;
+  HTML.timeleft = 5;
+  HTML.coundownP = document.querySelector(".countdown");
   HTML.numberOfStu = document.querySelector("#students");
   HTML.needle = document.querySelector(".needle");
-  setInterval(getData, 5000);
+  setInterval(countDown, 1000);
+  setInterval(getData, 6000);
+}
+function countDown() {
+  if (HTML.timeleft <= 0) {
+    clearInterval(countDown);
+  }
+  HTML.coundownP.textContent = 5 - HTML.timeleft;
+  HTML.timeleft -= 1;
 }
 async function getData() {
   const myQueue = await fetch(data);
@@ -20,5 +30,4 @@ function showQueue(queue) {
   console.log("show queue");
   console.log(HTML.inqueue.inQueue);
   HTML.numberOfStu.innerHTML = queue.inQueue + " students";
-  HTML.needle.classList.add("move");
 }
